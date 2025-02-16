d7d1d6bcdcb7295b0e2a618bf517e8f3c54aea1a
Subham3256 <subhambh2015@gmail.com>
2025-02-16 21:39:18 +0530

    Adi babu

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// 1.How do you loop through an array in JavaScript using fo?
+// let arr = [10, 20, 30, 40];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// 2.How do you find the length of an array?
+
+// let arr = [10, 20, 30,40,50,60,70,80,90];
+// console.log(arr.length); 
+
+
+// 3.How do you remove the last element of an array?
+// let arr = [10, 20, 30];
+// arr.pop();
+// console.log(arr); 
+
+
+
+// 4.How do you remove the first element of an array?
+// let arr = [10, 20, 30,40,50];
+// arr.shift();
+// console.log(arr); 
+
+// 5.How do you add elements to the beginning of an array?
+
+// let arr = [10, 20];
+// arr.unshift(5);
+// console.log(arr); 
+
+
+//6. How do you merge two arrays?
+
+
+
+//7. How do you check if an element exists in an array?
+
+
+
+// 8.How do you reverse an array?
+// let arr = [1, 2, 3];
+// arr.reverse();
+// console.log(arr); 
+
+// 9.How do you generate an array of numbers using loops?
+// let numbers = [];
+// for (let i = 1; i <= 5; i++) {
+//     numbers.push(i);
+// }
+// console.log(numbers); 
+
+
+// 10.How do you sort an array in ascending order?
+
